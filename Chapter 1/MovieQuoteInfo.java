d214df9a02719f2c0a29e27ba5b02079d9054efb
janceeena <jancenv92@gmail.com>
2024-08-15 22:00:12 -1200

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo {
+    public static void main (String[] args){
+
+        System.out.println("Quote: \"People ask me what my greatest strengths are and I say perspective. The best way to get that is to meet people that are polar opposites; you learn the most from them. There are pieces of you that are inherently yours, but everything else is a collection of the things you’ve seen and the people you’ve met.\"");
+        System.out.println("Title: Fresh Off The Boat (TV Series)");
+        System.out.println("Character: Eddie Huang");
+        System.out.println("Year: 2015");
+
+    }
+}
