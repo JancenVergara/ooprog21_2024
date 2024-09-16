f0e20333060227acd3b09e33a48a9414e999fa00
JancenVergara <jancenv92@gmail.com>
2024-09-16 20:37:37 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+    
+    public static void displayAddress() {
+        System.out.println("Company: Apple Inc.");
+        System.out.println("One Apple Park Way");
+        System.out.println("Cupertino, CA 95014");
+        System.out.println("United States of America");
+    }
+    
+    public static void main(String[] args) {
+        displayAddress();
+    }
+}
